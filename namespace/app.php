34d729c511a6ba20dee2ec6ba74859074365df15
Alemayehu Abebe <alexsabebe28@gmail.com>
2024-08-01 09:17:07 +0300

    last commit

---
+++ namespace/app.php
@@ -0,0 +1,10 @@
+<?php 
+ 
+ include("helper.php");
+ class Display{
+    public function Info(){
+        echo "this is the namespace demonsration code";
+    }
+ }
+
+?>
---
+++ namespace/helper.php
@@ -0,0 +1,8 @@
+<?php
+namespace App;
+class Display{
+    public function Info(){
+        echo "this is the namespace demonsration code";
+    }
+ }
+ ?>
